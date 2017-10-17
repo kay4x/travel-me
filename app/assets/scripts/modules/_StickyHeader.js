2909dd0ab9ac42d539b7cf60e225d30645c16047
koyedeji <koyedeji.ng@gmail.com>
2017-10-18 00:51:34 +0100

    completed sticky-header

---
+++ app/assets/scripts/modules/_StickyHeader.js
@@ -0,0 +1,62 @@
+import $ from "jquery";
+import waypoint from "../../../../node_modules/waypoints/lib/noframework.waypoints";
+import smoothScroll from "jquery-smooth-scroll";
+
+
+
+class StickyHeader {
+     constructor(options) {
+       this.siteHeader = options.siteHeader;
+       this.triggerElement = options.triggerElement;
+       this.primaryNavLink = options.primaryNavLink;
+       this.pageSections = options.pageSections;
+       this.headerLinks = options.headerLinks;
+
+      this.createHeaderWaypoints();
+      this.createPageSectionWaypoints();
+      this.headerLinks.smoothScroll();
+     }
+
+    createHeaderWaypoints() {
+        const that = this;
+        new Waypoint({
+          element: that.triggerElement[0],
+          handler(direction) {
+            const dark = "site-header--dark";
+            if(direction === "down") $(that.siteHeader).addClass(dark);
+            if(direction === "up") $(that.siteHeader).removeClass(dark);
+           }
+        });
+     }
+    createPageSectionWaypoints() {
+      var that = this;
+      this.pageSections.each(function(){
+        const currentSection = this;
+        new Waypoint({
+          element: currentSection,
+          handler(direction) {
+            if(direction === "down") {
+              let matchingHeaderLink = currentSection.getAttribute("data-matching-link");
+              that.headerLinks.removeClass("is-current-link");
+              $(matchingHeaderLink).addClass("is-current-link");
+            }
+        },
+        offset: "18%"
+        });
+        new Waypoint({
+          element: currentSection,
+          handler(direction) {
+            if(direction === "up") {
+              let matchingHeaderLink = currentSection.getAttribute("data-matching-link");
+              that.headerLinks.removeClass("is-current-link");
+              $(matchingHeaderLink).addClass("is-current-link");
+            }
+        },
+        offset: "40%"
+        });
+      });
+    }
+}
+
+
+export default StickyHeader;
